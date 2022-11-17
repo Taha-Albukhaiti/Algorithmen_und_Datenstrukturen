bf390624d64e64f47cdef320e5cc545c8b511bf9
Taha Al-Bukhaiti <taha.al-bukhaiti@stud.th-luebeck.de>
2022-11-17 17:23:02 +0100

    §

---
+++ src/main/java/datastructures/CountingSort.java
@@ -0,0 +1,32 @@
+package datastructures;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static int[] countingSort(int[] a){
+        int[] b = new int[a.length];
+        int beg = a[0];
+        for (int i = 1; i < a.length; i++){
+            if (a[i] > beg) beg = a[i];
+        }
+        beg++;
+        int[] c = new int[beg];
+        for (int l = 0; l < beg; l++) c[l] = 0;
+
+        for (int l = 0; l < a.length; l++){
+            c[a[l]] = c[a[l]] + 1 ;
+        }
+        for (int j = 1; j < beg; j++) c[j] = c[j] + c[j-1];
+
+        for (int k = a.length - 1; k >= 0; k--){
+            b[c[a[k]]] = a[k];
+            c[a[k]] = c[a[k]] - 1;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = countingSort(new int[]{3,1, 3, 2, 8, 5, 2});
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
--- src/main/java/datastructures/QuickSort.java
+++ src/main/java/datastructures/QuickSort.java
@@ -13,24 +13,25 @@ public class QuickSort {
 
     public static int partition(int[] arr, int p, int r) {
         int x = arr[r];
-        int i = p - 1;
+        int i = p;
         for (int j = p; j < r; j++) {
             if (arr[j] <= x) {
-                i++;
                 int tempo = arr[i];
                 arr[i] = arr[j];
                 arr[j] = tempo;
+                i++;
             }
         }
 
         int tempo2 = arr[r];
-        arr[r] = arr[i + 1];
-        arr[i + 1] = tempo2;
-        return i +1;
+        arr[r] = arr[i];
+        arr[i] = tempo2;
+
+        return i;
     }
 
     public static void main(String[] args) {
-        int[] arr = {9, 3, 22, 49, 100, 2, 1, 12, 4};
+        int[] arr = {17, 0, 9, 3, 22, 49, 100, 2, 1, 12, 4}; // 3 2 1
         quickSort(arr, 0, arr.length - 1);
         Arrays.stream(arr).forEach(System.out::println);
     }
