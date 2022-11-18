a40a14a1f98621d5262219ca3a95ef9b8d92ac6b
Taha Al-Bukhaiti <taha.al-bukhaiti@stud.th-luebeck.de>
2022-11-18 18:48:02 +0100

    §§

--- src/main/java/datastructures/CountingSort.java
+++ src/main/java/datastructures/CountingSort.java
@@ -4,24 +4,25 @@ import java.util.Arrays;
 
 public class CountingSort {
     public static int[] countingSort(int[] a){
-        int[] b = new int[a.length];
+        int[] b = new int[a.length + 1];
         int beg = a[0];
-        for (int i = 1; i < a.length; i++){
+        for (int i = 1; i < a.length; i++)
             if (a[i] > beg) beg = a[i];
-        }
+
         beg++;
         int[] c = new int[beg];
+
         for (int l = 0; l < beg; l++) c[l] = 0;
 
-        for (int l = 0; l < a.length; l++){
-            c[a[l]] = c[a[l]] + 1 ;
-        }
+        for (int i : a) c[i] = c[i] + 1;
+
         for (int j = 1; j < beg; j++) c[j] = c[j] + c[j-1];
 
-        for (int k = a.length - 1; k >= 0; k--){
+        for (int k = a.length -1; k >= 0; k--){
             b[c[a[k]]] = a[k];
             c[a[k]] = c[a[k]] - 1;
         }
+
         return b;
     }
 
